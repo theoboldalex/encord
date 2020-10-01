6d56c6154c4f0b968aba22fc985066de79bc5878
alex theobold <theoboldalex@gmail.com>
2020-10-01 10:36:44 +0100

    implemented screenCall function and filtered connections to allow only one connection

--- public/js/receiver.js
+++ public/js/receiver.js
@@ -9,7 +9,15 @@ const cinema = document.getElementById('cinema');
 
 // answer call from sender
 peer.on('call', (call) => {
+  // only allow one connection
+  for (const conn in peer.connections) {
+    if (peer.connections[conn].length > 1) {
+      peer.connections[conn].shift();
+    }
+  }
+
   call.answer();
+
   const myVideo = document.createElement('video');
 
   call.on('stream', (stream) => {
